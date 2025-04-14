4582a1c92c2cba4e9cb81f3573bb9f55a0098ad1
Andres6936 <andres6936@gmail.com>
2025-04-13 22:09:04 -0500

    Refactor AddNewProductModal to use Sheet component
    Replaced Dialog with Sheet for improved modal handling and flexibility. Updated state management and added snap point configurations to enhance user experience. Introduced memoized SheetContents and InnerSheet components for better performance.

--- app-scanner/modals/AddNewProductModal.tsx
+++ app-scanner/modals/AddNewProductModal.tsx
@@ -1,67 +1,119 @@
-import {X} from '@tamagui/lucide-icons'
-import {Button, Dialog, Paragraph, Unspaced,} from 'tamagui'
+import {ChevronDown, ChevronUp} from '@tamagui/lucide-icons'
+import {Button, H2, Input, Paragraph, Sheet, SheetProps, YStack} from 'tamagui'
 import NiceModal, {useModal} from "@ebay/nice-modal-react";
+import React, {memo} from "react";
+
+
+const spModes = ['percent', 'constant', 'fit', 'mixed'] as const
 
 export default NiceModal.create(() => {
     // Use a hook to manage the modal state
     const modal = useModal();
 
+    const [position, setPosition] = React.useState(0)
+    const [open, setOpen] = React.useState(false)
+    const [innerOpen, setInnerOpen] = React.useState(false)
+    const [snapPointsMode, setSnapPointsMode] =
+        React.useState<(typeof spModes)[number]>('percent')
+    const [mixedFitDemo, setMixedFitDemo] = React.useState(false)
+
+    const isPercent = snapPointsMode === 'percent'
+    const isConstant = snapPointsMode === 'constant'
+    const isFit = snapPointsMode === 'fit'
+    const isMixed = snapPointsMode === 'mixed'
+    const snapPoints = isPercent
+        ? [85, 50, 25]
+        : isConstant
+            ? [256, 190]
+            : isFit
+                ? undefined
+                : mixedFitDemo
+                    ? ['fit', 110]
+                    : ['80%', 256, 190]
+
     return (
-        <Dialog
-            modal
+        <Sheet
+            forceRemoveScrollEnabled={modal.visible}
+            modal={modal.visible}
             open={modal.visible}
-            onOpenChange={open => open ? modal.show() : modal.remove()}
+            dismissOnSnapToBottom
+            zIndex={100_000}
+            animation="medium"
         >
-            <Dialog.Portal>
-                <Dialog.Overlay
-                    key="overlay"
-                    animation="slow"
-                    opacity={0.5}
-                    enterStyle={{opacity: 0}}
-                    exitStyle={{opacity: 0}}
-                />
+            <Sheet.Overlay
+                animation="lazy"
+                backgroundColor="$shadow6"
+                enterStyle={{opacity: 0}}
+                exitStyle={{opacity: 0}}
+            />
 
-                <Dialog.Content
-                    bordered
-                    elevate
-                    key="content"
-                    animateOnly={['transform', 'opacity']}
-                    animation={[
-                        'quicker',
-                        {
-                            opacity: {
-                                overshootClamping: true,
-                            },
-                        },
-                    ]}
-                    enterStyle={{x: 0, y: -20, opacity: 0, scale: 0.9}}
-                    exitStyle={{x: 0, y: 10, opacity: 0, scale: 0.95}}
-                    gap="$4"
-                    marginHorizontal="$4"
-                >
-                    <Dialog.Title size="$6" fontWeight="bold">Cambio de contraseña</Dialog.Title>
-                    <Dialog.Description>
-                        Por favor, ingresa tu contraseña actual y la nueva contraseña que deseas establecer.
-                    </Dialog.Description>
-
-                    <Paragraph>
-                        Hello
-                    </Paragraph>
-
-                    <Unspaced>
-                        <Dialog.Close asChild>
-                            <Button
-                                position="absolute"
-                                top="$3"
-                                right="$3"
-                                size="$2"
-                                circular
-                                icon={X}
-                            />
-                        </Dialog.Close>
-                    </Unspaced>
-                </Dialog.Content>
-            </Dialog.Portal>
-        </Dialog>
+            <Sheet.Handle/>
+            <Sheet.Frame padding="$4" justifyContent="center" alignItems="center" gap="$5">
+                <SheetContents {...{modal, isPercent, innerOpen, setInnerOpen, setOpen}} />
+            </Sheet.Frame>
+        </Sheet>
     )
 })
+
+// in general good to memoize the contents to avoid expensive renders during animations
+const SheetContents = memo(
+    ({modal, isPercent, innerOpen, setInnerOpen, setOpen}: any) => {
+        return (
+            <>
+                <Button size="$6" circular icon={ChevronDown} onPress={() => setOpen(false)}/>
+                <Input width={200}/>
+                {modal && isPercent && (
+                    <>
+                        <InnerSheet open={innerOpen} onOpenChange={setInnerOpen}/>
+                        <Button
+                            size="$6"
+                            circular
+                            icon={ChevronUp}
+                            onPress={() => setInnerOpen(true)}
+                        />
+                    </>
+                )}
+            </>
+        )
+    }
+)
+
+function InnerSheet(props: SheetProps) {
+    return (
+        <Sheet animation="medium" modal snapPoints={[90]} dismissOnSnapToBottom {...props}>
+            <Sheet.Overlay
+                animation="medium"
+                bg="$shadow2"
+                enterStyle={{opacity: 0}}
+                exitStyle={{opacity: 0}}
+            />
+
+            <Sheet.Handle/>
+            <Sheet.Frame flex={1} justifyContent="center" alignItems="center" gap="$5">
+                <Sheet.ScrollView>
+                    <YStack padding="$5" gap="$8">
+                        <Button
+                            size="$6"
+                            circular
+                            alignSelf="center"
+                            icon={ChevronDown}
+                            onPress={() => props.onOpenChange?.(false)}
+                        />
+
+                        <H2>Hello world</H2>
+                        {[1, 2, 3, 4, 5, 6, 7, 8].map((i) => (
+                            <Paragraph key={i} size="$8">
+                                Eu officia sunt ipsum nisi dolore labore est laborum laborum in esse ad
+                                pariatur. Dolor excepteur esse deserunt voluptate labore ea. Exercitation
+                                ipsum deserunt occaecat cupidatat consequat est adipisicing velit
+                                cupidatat ullamco veniam aliquip reprehenderit officia. Officia labore
+                                culpa ullamco velit. In sit occaecat velit ipsum fugiat esse aliqua dolor
+                                sint.
+                            </Paragraph>
+                        ))}
+                    </YStack>
+                </Sheet.ScrollView>
+            </Sheet.Frame>
+        </Sheet>
+    )
+}
